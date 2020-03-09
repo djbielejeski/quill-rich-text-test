000310f5db909e5a9fb3a6be16bf6d368bd95b1c
David B <djbielejeski@gmail.com>
2020-03-09 08:59:56 -0500

    DB:  converted quill successfully to our model.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,11 +24,18 @@ export class AppComponent {
   dataModel = {
     ops: [
     {
-      insert: 'Test\nBullet 1'
+      insert: 'Test\n'
+    },
+    {
+      insert: 'Bullet 1'
     },
     {
       attributes: { list: 'bullet' },
-      insert: '\n\n'
+      insert: '\n'
+    },
+    {
+      attributes: { list: 'bullet' },
+      insert: '\n'
     },
     {
       insert: 'asbv'
@@ -56,7 +63,7 @@ export class AppComponent {
         {
           link: 'www.google.com'
         },
-      insert: 'Test'
+      insert: 'Link'
     },
     {
       insert: '\n'
@@ -80,69 +87,199 @@ export class AppComponent {
     }]
   };
 
+  private usedBulletsIndex: number[] = [];
+
+  convertBackToQuill() {
+    let currentModel = this.convertedDataModel;
+
+    let response = [];
+
+
+    this.dataModel = { ops: response };
+  }
+
 
   get convertedDataModel(): CustomTextModel[] {
-    let response: CustomTextModel[] = [];
-    const ops = this.dataModel.ops;
 
-    let endOfDeltaIndexes: number[] = [];
-    for (let i = 0; i < ops.length; i++) {
-      if (ops[i].insert.endsWith('\n')) {
-        endOfDeltaIndexes.push(i);
-      }
-    }
+    this.usedBulletsIndex = [];
 
-    console.log(endOfDeltaIndexes);
-
-    let startingIndex = 0;
-    endOfDeltaIndexes.forEach((deltaIndex) => {
-
-    });
-
-    return response;
+    // We need to parse all of the text based on \n
+    // a \n signifies the end of operations applied to a specific item
+    // Bullet points apply backwards
     /*
+    Text output:
+    - Bullet 1
+    -
+
+    Data
+    {
+      insert: 'Bullet 1'
+    },
+    {
+      attributes: { list: 'bullet' },
+      insert: '\n\n'
+    },
+
+    is equivalent to
+
+    Text output:
+    - Bullet 1
+    -
+
+    Data
+    {
+      insert: 'Bullet 1'
+    },
+    {
+      attributes: { list: 'bullet' },
+      insert: '\n'
+    },
+    {
+      attributes: { list: 'bullet' },
+      insert: '\n'
+    },
+     */
+
+
+    /*
+
+    {
+      insert: 'Test\n'
+    },
+    {
+      insert: 'Bullet 1'
+    },
+
+    is equivalent to
+
+    {
+      insert: 'Test\nBullet 1'
+    },
+
+     */
+
+    console.info('---------- Staring parsing ----------');
+
     let response: CustomTextModel[] = [];
     const ops = this.dataModel.ops;
 
+
+    let appliedBulletForLine: boolean = false;
     for(let i = 0; i < ops.length; i++) {
       let item = ops[i];
 
-      if (item) {
-        // Newline
-        if (item.insert === '\n' && !item.attributes) {
-          response.push({text: '', isNewLine: true});
+      let text = '' + item.insert;
+
+      let textSplitOnNewlines = this.getTextSplitOnNewlines(text);
+
+      console.log(textSplitOnNewlines);
+
+
+      for(let j = 0; j < textSplitOnNewlines.length; j++) {
+        let subtext = textSplitOnNewlines[j];
+
+        if (subtext == '') {
+          let isBullet = item.attributes && item.attributes.list == 'bullet' ? true : false;
+          let bulletNotAppliedBackwards = this.usedBulletsIndex.indexOf(i) == -1;
+
+          if (isBullet && bulletNotAppliedBackwards) {
+            response.push({text: '', isBulletPoint: true });
+            // Push an empty text node in
+            response.push({text: '' });
+          }
+
+          // Push the newline
+          response.push({text: '', isNewLine: true });
+
+          appliedBulletForLine = false;
         }
         else {
+          let model: CustomTextModel = {
+            text: subtext
+          };
 
-          // Bullet Points - These are weird, as the bullet comes AFTER the text node
-          // Read ahead once, for the first bullet point
-          let lookAheadIndex = i + 1;
-
-          if (lookAheadIndex < ops.length &&
-              ops[lookAheadIndex] &&
-              ops[lookAheadIndex].attributes &&
-              ops[lookAheadIndex].attributes.list == 'bullet') {
-
-            response.push({text: '', isNewLine: true});
-
-            // Insert our bullet point
-            response.push({ text: '', isBulletPoint: true });
-
-            // now get the content of the text node and put that after the bullet point
-            this.getCustomModelForTextNode(item, response);
-
-            // Skip past the next node (which is the bullet point)
-            i++;
+          if (item.attributes && item.attributes.bold) {
+            model.isBold = true;
           }
-          else {
-            this.getCustomModelForTextNode(item, response);
+
+          // Underline
+          if (item.attributes && item.attributes.underline) {
+            model.isUnderline = true;
           }
+
+          // Hyperlink
+          if (item.attributes && item.attributes.link) {
+            model.hyperlink = item.attributes.link;
+            model.isHyperLink = true;
+          }
+
+          // see if we are a bullet
+          let isBullet = this.isNodeAfterMeFinalizingThisTextNodeAndIsBullet(i);
+
+          if (isBullet && !appliedBulletForLine) {
+            appliedBulletForLine = true;
+
+            response.push({text: '', isBulletPoint: true });
+          }
+
+          response.push(model);
         }
       }
-    };
+    }
 
-    return response;*/
+
+    return response;
   }
+
+  private isNodeAfterMeFinalizingThisTextNodeAndIsBullet(index): boolean {
+    const ops = this.dataModel.ops;
+    let response: boolean = false;
+
+    for(let i = index; i < ops.length; i++) {
+      let item = ops[i];
+
+      if (item.insert.endsWith('\n')) {
+        response = item.attributes && item.attributes.list == 'bullet' ? true : false;
+
+        if(response) {
+          this.usedBulletsIndex.push(i);
+        }
+
+        i = ops.length;
+      }
+    }
+
+    return response;
+  }
+
+
+  private getTextSplitOnNewlines(text): string[] {
+    let response: string[] = [];
+
+    let indexOfNewline = text.indexOf('\n');
+    if (indexOfNewline == -1) {
+      response.push(text);
+    }
+    else {
+      let textWithoutNewline = text.substr(0, indexOfNewline);
+      if (textWithoutNewline) {
+        response.push(textWithoutNewline);
+      }
+
+      text = text.substr(textWithoutNewline + 2, text.length - (textWithoutNewline + 2));
+
+      if (text.indexOf('\n') != -1) {
+        response.push(...this.getTextSplitOnNewlines(text));
+      }
+      else {
+        // this is for the ending newline that this line has.
+        response.push('');
+      }
+    }
+
+    return response;
+  }
+
 
   private getCustomModelForTextNode(item, response: CustomTextModel[]) {
     let textSplitOnNewlines = item.insert.split('\n');
